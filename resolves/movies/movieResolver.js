8d2bbd468e42128e719016d4e8bedf067f54f1e4
Gorohov <d.gorohov@itransition.com>
2022-02-01 09:33:25 +0300

    add resolver

--- resolves/movies/movieResolver.js
+++ resolves/movies/movieResolver.js
@@ -1,7 +1,25 @@
+const axios = require("axios");
+const request = require("../../request");
+
 module.exports = {
-    trendingMovies: async () => {
-        return {
-            results: []
-        }
-    }
-}
+  trendingMovies: async () => {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3${request.fetchTrending}`
+    );
+    const { data } = response;
+
+    return {
+      ...data,
+    };
+  },
+  topMovies: async () => {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3${request.fetchTopRated}`
+    );
+    const { data } = response;
+
+    return {
+      ...data,
+    };
+  },
+};
