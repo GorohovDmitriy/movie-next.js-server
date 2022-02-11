15b61f0fabdb656b8daac0a6210f0af8e3cf0cb0
Gorohov <d.gorohov@itransition.com>
2022-02-11 12:08:05 +0300

    heroku

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const resolvers = require("./resolves");
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+    console.log(`
+      🚀  Server is ready at ${url}
+      📭  Query at https://studio.apollographql.com/dev
+    `);
+  });
